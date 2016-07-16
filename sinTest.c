89458f969da2745ae765f11570b07f937a97d7e5
student <student@localhost.localdomain>
2016-07-16 02:47:30 -0600

    cleaned up the math on most functions

--- sinTest.c
+++ sinTest.c
@@ -11,6 +11,7 @@ int main(void){
 
   int i;
   double sinValues [MAX_ELEM];
+  double xval[MAX_ELEM];
   double totalTime=0.0;
   //initialization of struct pointers                                             
   struct SineInterPolateObj *first = NULL;
@@ -20,7 +21,7 @@ int main(void){
   struct InterpolationObject *test = (struct InterpolationObject *) malloc (sizeof(struct InterpolationObject *));
   
  
-  setSineVal(&first, &curr, &last,sinValues);
+  setCOSINE(&first, &curr, &last,sinValues,xval);
   
   if(BT){ // code to iterate and test if linked list is compilated correctly.
     fprintf(stderr,"IN LINKED LIST\n");
@@ -28,7 +29,7 @@ int main(void){
     curr = first;
     
     while (curr !=NULL){
-      if (BT) fprintf(stderr,"Node value: %f i:%d \n",curr->value, i++);
+      if (BT1) fprintf(stderr,"Node value: %f i:%d \n",curr->value, i++);
       last=curr;
       curr=curr->next;
       free(last);
@@ -36,13 +37,15 @@ int main(void){
     }
   }
   test->n=MAX_ELEM;
-  test->x0=1;
-  test->invDx=1/INCR;
+  test->x0=X_LOW;
+  test->invDx=INVDX;
   test->values = &sinValues;
   if(BT){
-    for(i=0;i<MAX_ELEM;i++)
-      printf("ARRAY X: %f\n",test->values[i]);
-    fprintf(stderr, "Num of objects:%d, Starting ordinate range: %f, table spacing: %f\n",test->n,test->x0, test->invDx); 
+    for(i=0;i<MAX_ELEM;i++){
+      printf("i: %d ARRAY cos(x*PI): %f\n",i,test->values[i]);
+      printf("i: %d ARRAY x*PI: %f\n",i,xval[i]);
+    }
+    fprintf(stderr, "Num of objects:%d\nStarting ordinate range: %f\ninverse table spacing: %f\n",test->n,test->x0, test->invDx); 
   }
   
 
@@ -51,31 +54,39 @@ int main(void){
   f=(double *)malloc(sizeof(double *));
   double *df;
   df=(double *)malloc(sizeof(double *));
-  double complete = 5;
-  int j;
+  double complete = 0.5;
+  int j,rVal;
   
   for(;totalTime < complete ; ){
     //fprintf(stderr,"j: %d\n",j);
     clock_gettime(CLOCK_REALTIME, &start);
     s=setRandNum(1,10000);
-    interpolate(test, s, f,df);
+    rVal=(int)floor(s);
+    if(BT){
+      printf("\n");
+      printf("***** Starting interpolate ******\n");
+      printf("random value generated: %f\n",s);
+      printf("test->table[%d]: %f\nxval: %f\n",rVal,test->values[rVal],xval[rVal] );
+    }
+    interpolate(test, xval[rVal], f,df);
+    printf("~~~~~ Interpolate complete ~~~~~\n");
+    printf("\n");
     
     clock_gettime(CLOCK_REALTIME, &stop);
     totalTime += ((double)(stop.tv_sec - start.tv_sec)) + ((double)(stop.tv_nsec - start.tv_nsec)) / BIL;
     // printf(" TIME: %f\n",totalTime);
   }
-   printf(" TIME: %f\n",totalTime);
-  
-
-  
-  printf("\n");
-  fprintf(stderr, "Start time: %lf\n",start);
-  printf("\n");
-  fprintf(stderr, "Total time: %lf\n",totalTime);
-  printf("\n");
-  fprintf(stderr, "Stop time: %lf\n",stop);
-  printf("\n");
-  printf("test\n");
-  fprintf(stderr," \n");
+  if(BT){
+    printf(" TIME: %f\n",totalTime);
+    printf("\n");
+    fprintf(stderr, "Start time: %lf\n",start);
+    printf("\n");
+    fprintf(stderr, "Total time: %lf\n",totalTime);
+    printf("\n");
+    fprintf(stderr, "Stop time: %lf\n",stop);
+    printf("\n");
+    printf("test\n");
+    fprintf(stderr," \n");
+  }
   return 0;
 }
--- sinTest.h
+++ sinTest.h
@@ -5,9 +5,13 @@
 #define MAX_ELEM 10000
 #define INCRM 0.036
 #define VERBOSE 0
-#define BT 0
+#define BT 1
+#define BT1 0
 #define RANGE 2.0
+#define X_LOW -1*M_PI
+#define X_HI 1*M_PI
 #define INCR (RANGE/MAX_ELEM)
+#define INVDX (1/(INCR*M_PI))
 
 /* Node for sin table linked list. */
 struct SineInterPolateObj{
@@ -27,15 +31,18 @@ param [in] dx   spacing of the ordinate values. {1.0/dx};
 param [in] data abscissa values.  An array of size n. 
 */
 struct InterpolationObject{
-   int n;          //!< the number of values in the table                         
-   double x0;      //!< the starting ordinate range                               
-   double invDx;   //!< the inverse of the table spacing                          
-   double *values; //!< the abscissa values values=                                     
-} ;
+   int n;          //!< the number of values in the table                       
+   double x0;      //!< the starting ordinate range                             
+   double invDx;   //!< the inverse of the table spacing                        
+   double *values; //!< the abscissa values values=               
+};
+
+double getXVal_N(double r){
+  return (double)(r*((2*M_PI)/MAX_ELEM)-M_PI);
+}
 
 
-
-void setCOSINE(struct SineInterPolateObj *first, struct SineInterPolateObj *curr, struct SineInterPolateObj *last, double *sinValues){
+void setCOSINE(struct SineInterPolateObj *first, struct SineInterPolateObj *curr, struct SineInterPolateObj *last, double *sinValues, double *xvals){
    
   int i;
   double x = -1.0; // Temporary value of x to run through sine function        
@@ -53,8 +60,9 @@ void setCOSINE(struct SineInterPolateObj *first, struct SineInterPolateObj *curr
     curr->value = cos(x * M_PI); // insert sin value into node for linked list
     curr->next = NULL; // set next to null
     last=curr; // set last to current
-
+    
     sinValues[i]=cos(x * M_PI); // insert values into table array
+    xvals[i]=x*M_PI;
     x+=INCR; // increment value of x
   }
   
@@ -88,25 +96,48 @@ void setSineVal(struct SineInterPolateObj **first, struct SineInterPolateObj *cu
 void interpolate(struct InterpolationObject* table, double r, double* f, double* df) {
    const double* tt = table->values; // alias
    
-   if ( r < table->x0 ) r = table->x0;
+   if(BT) fprintf(stderr, "r passed in: %f \n",r);
 
-   r = (r-table->x0)*(table->invDx) ;
-   int ii = (int)floor(r);
+   if(BT) printf("r < table->x0 : %s\n",(r < table->x0)?"true":"false");
+   if ( r < table->x0 ){ r = table->x0;}
 
-   if (ii > table->n) {
-      ii = table->n;
-      r = table->n / table->invDx;
+   r = (r-table->x0)*(table->invDx);
+   printf("r= (r-table->x0)*(table->invDx): %f\n",(r-table->x0)*(table->invDx));
+   
+   int ii;
+   ii = (int)floor((int)r);
+   if(BT1) printf("intitial ii val: %d\n",ii);
+   if(BT){
+     fprintf(stderr,"r: %f\n",r);
+     fprintf(stderr,"ii: %d\n",ii);
    }
-
-   r = r - floor(r); // reset r to fractional distance                            
+   if (ii > table->n) {
+     ii = table->n;
+     r = table->n / table->invDx;
+   }
+   
+   r = r - floor(r); // reset r to fractional distance
+   printf("r reset to fractional dist: %f\n",r - floor(r));
+   if(BT){
+     fprintf(stderr,"After bounds checking\n");
+     fprintf(stderr,"r: %f\n",r);
+     fprintf(stderr,"ii: %d\n",ii);
+   }
+   
    double g1 = tt[ii+1] - tt[ii-1];
-
    double g2 = tt[ii+2] - tt[ii];
+
+   if(BT){
+     printf("g1:%f = tt[%d]:%f - tt[%d]:%f\n",g1,ii+1,tt[ii+1],ii-1,tt[ii-1]);
+     printf("g2:%f = tt[%d]:%f - tt[%d]:%f\n",g2,ii+2,tt[ii+2],ii,tt[ii]);
+   }
    *f = tt[ii] + 0.5*r*(g1 + r*(tt[ii+1] + tt[ii-1] - 2.0*tt[ii]) );
    *df = 0.5*(g1 + r*(g2-g1))*table->invDx;
-   //fprintf(stderr,"%f\n",f);
+   if(BT){
+     fprintf(stderr,"f(x): %f\n",f);
+     fprintf(stderr,"df(x): %df\n",df);
+   }
 }
-
 
 double setRandNum(double minN, double maxN){
   return (double) rand()/RAND_MAX * (maxN - minN) + minN;
