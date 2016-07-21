82b8399552a9c258e73e2f055a5f553d5f6dcbed
student <student@localhost.localdomain>
2016-07-21 00:53:06 -0600

    idk what to put down

--- loadModule.sh
+++
@@ -1,4 +0,0 @@
-clear
-printf "Loading module\n"
-module load mpi/openmpi-x86_64
-printf "Module load complete\n"
--- testSim.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh                                                                       
-
-../bin/CoMD-mpi -e -x 20 -y 20 -z 20
